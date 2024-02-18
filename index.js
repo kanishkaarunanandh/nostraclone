a4f3658f4fcb20653cee2410738fdcf7c5d0d275
kanishkaarunanandh <kanishka0000012@gmail.com>
2024-02-18 13:48:50 +0530

    Fix line endings

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var sidenav = document.querySelector(".side-navbar");
 
 function showNavbar() {
-    sidenav.style.left = "0";
+    sidenav.style.display ="block"
 }
 
 function closeNavbar() {
-    sidenav.style.left = "-60%";
+    sidenav.style.display ="none"
 }
